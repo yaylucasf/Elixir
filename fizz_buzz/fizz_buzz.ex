afbb4e7b725d37d4d8eae31c38fe625a91a93c7d
Lucas Fernandes <124588230+yaylucasf@users.noreply.github.com>
2023-03-03 14:46:06 -0300

    Code Refactoring

--- fizz_buzz/fizz_buzz.ex
+++ fizz_buzz/fizz_buzz.ex
@@ -1,44 +1,31 @@
 defmodule FizzBuzz do
 
-  def main(file) do
+    def main(file) do
     
-    file
-    |> File.read()
-    |> get_read_file_return()
-    |> String.trim()
-    |> split(",")
-    |> apply_fizz_buzz()
-
-  end
-
-  def get_read_file_return({:ok, result}) do
-    result
-  end
-
-  def get_read_file_return({:error, reason}) do
-    reason
-  end
-
-  def split(string, separator) do
-    String.split(string, separator)
-  end
-
-  def fizz_buzz(x) do
-
-    cond do
-      (div(x, 3) * 3 == x) and (div(x, 5) * 5 != x) -> "Fizz"
-      (div(x, 3) * 3 != x) and (div(x, 5) * 5 == x) -> "Buzz"
-      (div(x, 3) * 3 == x) and (div(x, 5) * 5 == x) -> "FizzBuzz"
-      true -> x
+        file
+        |> File.read()
+        |> get_result()
+        |> String.trim()
+        |> String.split(",")
+        |> Enum.map(&String.to_integer/1)
+        |> Enum.map(&fizz_buzz/1)
+        
     end
 
-  end
+	  def get_result({:ok, result}), do: result
+	  def get_result({:error, reason}), do: reason
 
-  def apply_fizz_buzz(list) do
-
-    Enum.map(list, &String.to_integer/1)
-    |> Enum.map(&fizz_buzz/1)
-
-  end
-
+    def fizz_buzz(x) do
+    
+		cond do
+    
+		    div(x, 3) * 3 == x and div(x, 5) * 5 != x -> "Fizz"
+		    div(x, 3) * 3 != x and div(x, 5) * 5 == x -> "Buzz"
+		    div(x, 3) * 3 == x and div(x, 5) * 5 == x -> "FizzBuzz"
+			true -> x
+      
+    end
+    
+    end    
+    
 end
