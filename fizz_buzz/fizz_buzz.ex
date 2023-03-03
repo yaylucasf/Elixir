3d0523f1d8a4f3c9e4a24de9c49813463bd3492f
Lucas Fernandes <124588230+yaylucasf@users.noreply.github.com>
2023-03-03 16:25:21 -0300

    Refactoring

--- fizz_buzz/fizz_buzz.ex
+++ fizz_buzz/fizz_buzz.ex
@@ -1,31 +1,34 @@
 defmodule FizzBuzz do
 
-    def main(file) do
-    
-        file
-        |> File.read()
-        |> get_result()
-        |> String.trim()
-        |> String.split(",")
-        |> Enum.map(&String.to_integer/1)
-        |> Enum.map(&fizz_buzz/1)
-        
+  def main(file) do
+
+    file |> File.read() |> get_file_read_result() |> format()
+    |> Enum.map(&String.to_integer/1) |> Enum.map(&fizz_buzz/1)
+
+  end
+
+  def get_file_read_result({:ok, result}), do: result
+  def get_file_read_result({:error, reason}), do: reason
+
+  def format(:enoent), do: exit("This file doesn't exist!")
+
+  def format(string) do
+
+    string |> String.trim() |> String.split(",")
+
+  end
+
+  def fizz_buzz(x) do
+
+    cond do
+
+      (div(x, 3) * 3 == x) && (div(x, 5) * 5 != x) -> "Fizz"
+      (div(x, 3) * 3 != x) && (div(x, 5) * 5 == x) -> "Buzz"
+      (div(x, 3) * 3 == x) && (div(x, 5) * 5 == x) -> "FizzBuzz"
+      true -> x
+
     end
 
-	  def get_result({:ok, result}), do: result
-	  def get_result({:error, reason}), do: reason
+  end
 
-    def fizz_buzz(x) do
-    
-		cond do
-    
-		    div(x, 3) * 3 == x and div(x, 5) * 5 != x -> "Fizz"
-		    div(x, 3) * 3 != x and div(x, 5) * 5 == x -> "Buzz"
-		    div(x, 3) * 3 == x and div(x, 5) * 5 == x -> "FizzBuzz"
-			true -> x
-      
-    end
-    
-    end    
-    
 end
